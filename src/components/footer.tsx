a5a2861797b74394ee15a1e29d22d98ef8c926d3
Rodney Osodo <socials@rodneyosodo.com>
2026-02-13 19:14:27 +0300

    fix(footer): point legal links to external URLs and open in new tab
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -157,13 +157,25 @@ export default function FooterSection() {
             </span>
           </div>
           <div className="flex flex-wrap justify-center gap-4 text-xs">
-            <Link href="/terms" className="hover:underline">
+            <Link
+              href="https://www.absmach.eu/terms/"
+              target="_blank"
+              className="hover:underline"
+            >
               Terms of Service
             </Link>
-            <Link href="/privacy" className="hover:underline">
+            <Link
+              href="https://www.absmach.eu/privacy/"
+              target="_blank"
+              className="hover:underline"
+            >
               Privacy Policy
             </Link>
-            <Link href="/imprint" className="hover:underline">
+            <Link
+              href="https://www.absmach.eu/imprint/"
+              target="_blank"
+              className="hover:underline"
+            >
               Imprint
             </Link>
           </div>
